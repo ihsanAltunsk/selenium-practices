9c807eb1400bb7e05004dbf278ac105173980059
İhsan <altnskihsan@gmail.com>
2023-11-22 22:34:06 +0300

    11.22.23

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class deneme {
-        //Launch browser
+        // - Launch browser
         //2. Navigate to url 'http://automationexercise.com'
         //3. Verify that home page is visible successfully
 
