bdfd4418128e4afceb6692c3d2f41ed5fc3a5bdd
İhsan <altnskihsan@gmail.com>
2023-12-02 23:27:30 +0300

    12.02.23

---
+++ src/test/java/testCases/TablePractice_2.java
@@ -0,0 +1,59 @@
+package testCases;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TablePractice_2 extends TestBase {
+    //1. Go to "https://demoqa.com/webtables"
+    //2. Print the titles in Headers
+    //3. Print the title of the 3rd column
+    //4. Print all data in the table
+    //5. Print how many non-empty cells (data) are in the table
+    //6. Print the number of rows in the table
+    //7. Print the number of columns in the table
+    //8. Print the 3rd column in the table
+    //9. Print the Salary of the person whose "First Name" is Kierra in the table
+    //10. Create a method on the Page page, let me print the data when I enter the number of rows and columns on the Test page.
+
+    @Test
+    public void test01(){
+        //1. Go to "https://demoqa.com/webtables"
+        driver.get("https://demoqa.com/webtables");
+
+        //2. Print the titles in Headers
+        List<WebElement> headerElements = driver.findElements(By.xpath("//*[@role='columnheader']"));
+        List<String> headerElementsStr = ReusableMethods.convertToString(headerElements);
+        System.out.println(headerElementsStr);
+
+        //3. Print the title of the 3rd column
+        System.out.println(headerElementsStr.get(2));
+
+        //4. Print all data in the table
+        List<WebElement> allElements = driver.findElements(By.xpath("//*[@role='row']/*[@role='gridcell']"));
+        List<String> allElementsStr = ReusableMethods.convertToString(allElements);
+        List<String> actualAllElementsStr = new ArrayList<>();
+        for (int i = 0; i < allElementsStr.size();i++){
+            if (allElementsStr.get(i).equals(" ")){
+
+            }else {
+                actualAllElementsStr.add(i,allElementsStr.get(i));
+            }
+        }
+        System.out.println(actualAllElementsStr);
+
+        //5. Print how many non-empty cells (data) are in the table
+
+
+        //6. Print the number of rows in the table
+        //7. Print the number of columns in the table
+        //8. Print the 3rd column in the table
+        //9. Print the Salary of the person whose "First Name" is Kierra in the table
+        //10. Create a method on the Page page, let me print the data when I enter the number of rows and columns on the Test page.
+    }
+}
